8f638d983fd0a0f24047cd249f70327036c4a7ea
sanctityQ <q.sanctity@gmail.com>
2016-06-30 14:04:12 +0800

    spring mvc的一些测试暂时保留

--- server/src/main/java/org/one/xingyun/WebConfig.java
+++ server/src/main/java/org/one/xingyun/WebConfig.java
@@ -1,20 +1,31 @@
 package org.one.xingyun;
 
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configurable
-@EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+//@Configurable
+//@EnableWebMvc
+//@ComponentScan(basePackageClasses = {WebConfig.class } )
+public class WebConfig extends WebMvcConfigurerAdapter implements WebApplicationInitializer {
+
+  @Override
+  public void configureViewResolvers(ViewResolverRegistry registry) {
+//    registry.viewResolver();
+  }
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-    registry.addResourceHandler("/**")
-        .addResourceLocations("classpath:/static/,file:../client/client/build/");
+    registry.addResourceHandler("/**").addResourceLocations("file:../client/client/build/");
   }
 
   @Override
@@ -22,4 +33,22 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     registry.addViewController("/").setViewName("index");
   }
 
+  @Override
+  public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+    configurer.enable();
+  }
+
+  @Override
+  public void onStartup(ServletContext container) throws ServletException {
+
+    AnnotationConfigWebApplicationContext dispatcherContext =
+        new AnnotationConfigWebApplicationContext();
+    dispatcherContext.register(WebConfig.class);
+
+    DispatcherServlet dispatcherServlet = new DispatcherServlet(dispatcherContext);
+    ServletRegistration.Dynamic registration = container.addServlet("dispatcher", dispatcherServlet);
+    registration.setLoadOnStartup(1);
+
+    registration.addMapping("/");
+  }
 }
