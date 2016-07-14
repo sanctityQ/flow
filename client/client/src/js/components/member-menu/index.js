67a041c3cf79ee8968212e007850c1641e32f993
woshiwanting <smartwanting@gmail.com>
2016-07-14 19:09:42 +0800

    增加添加参与人列表

--- client/client/src/js/components/member-menu/index.js
+++ client/client/src/js/components/member-menu/index.js
@@ -13,7 +13,8 @@ class MemberMenu extends ClickAway(Component) {
     this.state = {
       active: false,
       selectedIndex: [],
-      memberList: []
+      memberList: [],
+      isSelectAll: false
     };
 
     this.open = this.open.bind(this);
@@ -47,6 +48,30 @@ class MemberMenu extends ClickAway(Component) {
     ))
   }
 
+  toggleMemberSelected(index) {
+    var selectedIndexClone = this.state.selectedIndex;
+
+    if (selectedIndexClone.indexOf(index) > -1) {
+      selectedIndexClone = selectedIndexClone.filter((item, i)=>{
+        return item !== index;
+      });
+    } else {
+      selectedIndexClone.push(index);
+    }
+
+    this.setState({
+      selectedIndex: selectedIndexClone,
+      isSelectAll: false
+    });
+  }
+
+  selectAll() {
+    this.setState({
+      isSelectAll: !this.state.isSelectAll,
+      selectedIndex: this.state.isSelectAll ? [] : this.state.memberList.map((v,i)=>(i))
+    });
+  }
+
   render() {
     return (
       <div className={classNames('popover', 'member-menu-view', 'bottom', 'in', {'active': this.state.active})} ref="menu">
@@ -55,20 +80,21 @@ class MemberMenu extends ClickAway(Component) {
             <input className="filter-input form-control" placeholder="查找成员" />
           </div>
           <ul className="list-unstyled thin-scroll">
-            <li className="member-item all active">
+            <li className={classNames('member-item', 'all', 'active', {'selected': this.state.isSelectAll})} onClick={()=>(this.selectAll())}>
               <a>
                 <i className="fa fa-users"></i>所有成员
               </a>
+              <i className="fa fa-check"></i>
             </li>
-            {this.state.memberList.map((member, i)=>{
-              <li className="member-item one hinted">
+            {this.state.memberList.map((member, i)=>(
+              <li className={classNames('member-item', 'one', 'hinted', {'selected': this.state.selectedIndex.indexOf(i) > -1})} key={i} onClick={()=>(this.toggleMemberSelected(i))}>
                 <a>
-                  <img className="avatar img-circle" src="{member.avatar}"/>
+                  <img className="avatar img-circle" src={member.avatar} />
                   <span>{member.name}</span>
                 </a>
                 <i className="fa fa-check"></i>
               </li>
-            })}
+            ))}
           </ul>
         </div>
       </div>
